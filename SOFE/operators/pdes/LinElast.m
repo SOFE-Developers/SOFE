8c3f6a749db803f7b7caeaff2a45346aacfbfdef
Lars <lars.ludwig@tu-dresden.de>
2017-02-06 11:40:27 +0100

    add demo cup.m, optional data.f in LinElast

--- SOFE/operators/pdes/LinElast.m
+++ SOFE/operators/pdes/LinElast.m
@@ -5,9 +5,14 @@ classdef LinElast < PDE
       lambda = data.E*data.nu/(1+data.nu)/(1-2*data.nu);
       sLap = Op_data_SYMGRAD_SYMGRAD(2*mu, fes);
       divDiv = Op_data_div_div(lambda, fes);
+      try
+        f = Fc_Data_Id(data.f, fes, 0);
+      catch
+        f = Fc_Data_Id(@(x)0*x(:,1), fes, 0);
+      end
       %
       lhs = {sLap, divDiv};
-      rhs = {Fc_Data_Id(data.f, fes, 0)};
+      rhs = {f};
       try % Neumann BC
         rhs{2} = Fc_Data_Id(data.g, fes, 1);
       end
---
+++ SOFE/work/demo/cup.m
@@ -0,0 +1,20 @@
+% DATA
+clear data;
+data.nu = 1/3;
+data.E = 1e6;
+data.g = @(x) bsxfun(@times, [0*x(:,1) 0*x(:,1) -1e4+0*x(:,1)], (x(:,2)>80-1e-6 & x(:,3)>25));
+data.dLoc = @(x) x(:,3)<-25;
+% MESH;
+m = SalomeMesh('cup');
+m.nBlock = 50;
+% ELEMENT
+e = PpL(3, 1);
+% FESPACE
+fes = FESpace(m, TPElem(e), data.dLoc);
+% PDE
+p = LinElast(data, fes);
+% SOLVE
+p.compute();
+% VISUALIZE
+v = Visualizer.create(fes); clf
+v.show(p.solution, 'p', struct('deform', true, 'n', 1)); view(30,40);
