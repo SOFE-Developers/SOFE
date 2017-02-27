6a4c20cf6798f664d6d4eaf7dcfc50059fff0977
Lars <lars.ludwig@tu-dresden.de>
2017-02-27 09:40:43 +0100

    multi-GlobalSearcher

--- SOFE/meshes/GlobalSearcher.m
+++ SOFE/meshes/GlobalSearcher.m
@@ -4,13 +4,24 @@ classdef GlobalSearcher < handle
     bgMesh
     diam
     NVec
+    nBlock
   end
   methods % constructor
-    function obj = GlobalSearcher(topology)
+    function obj = GlobalSearcher(topology, nBlock)
+      obj.nBlock = nBlock;
       obj.topology = topology;
-      fprintf('Set up GlobalSearcher ... ');
+      fprintf('Set up GlobalSearcher ... \n');
       obj.notify();
       fprintf('DONE\n');
+    end
+    function R = getBlock(obj, varargin) % [I]
+      nE = obj.topology.getNumber(obj.topology.dimP);
+      if obj.nBlock>nE, error('!Number of blocks exceeds number of elements!'); end
+      R = unique(floor(linspace(0,nE,obj.nBlock+1)));
+      R = [R(1:end-1)+1; R(2:end)];
+      if nargin > 1
+        R = R(:,varargin{1});
+      end
     end
     function notify(obj)
       dim = obj.topology.dimW;
@@ -29,10 +40,14 @@ classdef GlobalSearcher < handle
                     obj.NVec*range(3)^2/range(1)/range(2)].^(1/3);
       end
       obj.NVec = ceil(obj.NVec);
-      obj.bgMesh = obj.buildBackgroundMesh();
+      obj.bgMesh = cell(obj.nBlock,1);
+      for k = 1:obj.nBlock
+        idx = obj.getBlock(k);
+        obj.bgMesh{k} = obj.buildBackgroundMesh(idx(1):idx(2));
+      end
     end
-    function R = buildBackgroundMesh(obj)
-      elem = obj.topology.getEntity(obj.topology.dimP); % nExnV
+    function R = buildBackgroundMesh(obj, I)
+      elem = obj.topology.getEntity(obj.topology.dimP, I); % nExnV
       bins = zeros([size(elem) size(obj.topology.nodes,2)]); % nExnVxnW
       nV = size(elem,2);
       for k = 1:nV
@@ -44,7 +59,7 @@ classdef GlobalSearcher < handle
       nW = size(minBins, 2);
       XX = cell(nW, 1); iX = cell(nW, 1);
       for d = 1:nW
-        XX{d} = reshapeTop(1-minBins(:,d)+maxBins(:,d));
+        XX{d} = reshapeTop(1-minBins(:,d)+maxBins(:,d)); % n{CoveredBins}_dxnE
         iX{d} = XX{d}==0;
         XX{d} = bsxfun(@plus, XX{d}, minBins(:,d)'-1);
         XX{d}(iX{d}) = 0;
@@ -64,13 +79,13 @@ classdef GlobalSearcher < handle
         IDXE(:,3) = reshape(kron(XX{3}, ones(size(XX{1},1)*size(XX{2},1),1)), [], 1);
         IDXE(:,4) = reshape(kron((1:size(XX{1},2))', ones(size(XX{1},1)*size(XX{2},1)*size(XX{3},1),1)),[],1);
       end
-      IDXE = IDXE(prod(IDXE,2)>0,:);
+      IDXE = IDXE(prod(IDXE(:,1:nW),2)>0,:); % (n{CoveredBins}_d*nE)x(nW+1)
       L = obj.getLinearIndex(IDXE(:,1:nW));
-      IDXE = [L IDXE(:,nW+1)];
+      IDXE = [L IDXE(:,nW+1)]; % (n{CoveredBins}_d*nE)x2
       [IDXE(:,1), pVec] = sort(IDXE(:,1));
       IDXE(:,2) = IDXE(pVec,2);
       if ~exist('OCTAVE_VERSION', 'builtin')
-        [dmy, cnt] = unique(IDXE(:,1),'legacy');
+        [~, cnt] = unique(IDXE(:,1),'legacy');
       else
         [~, cnt] = unique(IDXE(:,1));
       end
@@ -82,9 +97,17 @@ classdef GlobalSearcher < handle
   methods % global search
     function R = findCandidates(obj, points)
       [~,L] = obj.getBin(points);
-      R = zeros(numel(L), size(obj.bgMesh,2));
       I = L>0 & L<=prod(obj.NVec);
-      R(I,:) = obj.bgMesh(L(I),:);
+      R = cell(1,obj.nBlock);
+      for k = 1:obj.nBlock
+        idx = obj.getBlock(k);
+        R{k} = zeros(numel(L), size(obj.bgMesh{k},2));
+        R{k}(I,:) = obj.bgMesh{k}(L(I),:);
+        R{k}(R{k}>0) = R{k}(R{k}>0) + (idx(1)-1);
+      end
+      R = cell2mat(R)';
+      colSum = sum(R>0);
+      R = reshapeTop(colSum, R(R>0))';
     end
     function [R, L] = getBin(obj, points)
       sPoints = bsxfun(@rdivide, bsxfun(@minus, points, obj.diam(:,1)'), diff(obj.diam')); % nPx3
