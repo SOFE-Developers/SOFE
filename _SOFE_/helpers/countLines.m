1561748fd92cbf7765a22bd51eae96d6e8635668
Lars <lars.ludwig@tu-dresden.de>
2017-03-23 15:45:49 +0100

    cosmetics @ helpers

--- _SOFE_/helpers/countLines.m
+++ _SOFE_/helpers/countLines.m
@@ -1,12 +1,12 @@
 function R = countLines(file)
   fid = fopen(file, 'rb');
-  %# Get file size.
+  % Get file size.
   fseek(fid, 0, 'eof');
   fileSize = ftell(fid);
   frewind(fid);
-  %# Read the whole file.
+  % Read the whole file.
   data = fread(fid, fileSize, 'uint8');
-  %# Count number of line-feeds and increase by one.
+  % Count number of line-feeds and increase by one.
   R = sum(data == 10) + 1;
   fclose(fid);
 end
