29fbde9e468ff5b0c5abae185a681e1e992be88d
Lars <lars.ludwig@tu-dresden.de>
2017-10-04 14:50:33 +0200

    NavierStokes:OpGradGrad->OpSGradSGrad

--- SOFE/core/operators/pde/NavierStokes.m
+++ SOFE/core/operators/pde/NavierStokes.m
@@ -1,7 +1,8 @@
 classdef NavierStokes < PDE
   methods % constructor
     function obj = NavierStokes(data, fesV, fesP)
-      lap = OpGradGrad(data.nu, fesV, fesV);
+%       lap = OpGradGrad(data.nu, fesV, fesV);
+      lap = OpSGradSGrad(data.nu, fesV);
       conv = OpGradId(@(x,t,U)U{1}, fesV, fesV);
       grad = OpIdDiv(@(x)1+0*x(:,1), fesP, fesV);
       div = OpDivId(@(x)1+0*x(:,1), fesV, fesP);
