e66758857eb8222be416af75d4f51daec502a20e
laaaars <lars.ludwig@tu-dresden.de>
2017-12-11 16:23:20 +0100

    work @ PDE2

--- SOFE/core/operators/pde/Poisson2.m
+++ SOFE/core/operators/pde/Poisson2.m
@@ -1,15 +1,12 @@
 classdef Poisson2 < PDE2
-% GradGrad(a) + Id_Gamma(h) = FId(f) + FId_Gamma(g)
   methods % constructor
     function obj = Poisson2(data, fesTrial, varargin)
+      if ~isempty(varargin), fesTest = varargin{:}; else, fesTest = fesTrial; end
       opList = {OpGradGrad(data.a, fesTrial, varargin{:}), ...
-                OpGradId([1 1], fesTrial, varargin{:})};
-      lhs = {{1,2}}; opFactor = {{1.0, 10.0}}; opAdj = {{0 0}};
-      %
-      fcList = {FcId(data.f, opList{1}.fesTest, 0)};
-      rhs = {{1}}; fcFactor = {{1}};
-      %
-      obj = obj@PDE2(lhs, rhs, opList, opFactor, opAdj, fcList, fcFactor);
+                FcId(data.f, fesTest, 0)};
+      lhs.sys = {{1}}; 
+      rhs.sys = {{2}};
+      obj = obj@PDE2(opList, lhs, rhs);
     end
   end
 end
