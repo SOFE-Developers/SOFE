758028be40fda0bb5d4bd6b1480b45d3f39c95f8
Ludwig, Lars <lars.ludwig@tu-dresden.de>
2018-05-11 08:44:02 +0200

    ...

--- SOFE/work/demo/laplaceBeltrami.m
+++ SOFE/work/demo/laplaceBeltrami.m
@@ -4,7 +4,7 @@ N = 50; order = 1; isTri = 1;
 data = struct('a',0.0025,'b',[1 0 0],'f',1);
 % MESH
 [nodes, elem] = Mesh.getTensorProductMesh({linspace(0,1,N),linspace(0,1,N)}, isTri);
-m = Mesh([nodes sin(2*pi*nodes(:,1)).*cos(2*pi*nodes(:,2))], elem, 2);
+m = Mesh([nodes 0.1*sin(2*pi*nodes(:,1)).*cos(2*pi*nodes(:,2))], elem, 2);
 % FESPACE
 if isTri, e = PpL(2, order); else, e = QpL(dim, order); end
 fes = FESpace(m, e, @(x) x(:,1) < eps);
