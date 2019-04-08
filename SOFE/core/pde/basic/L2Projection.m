31332a9e66d20a2c66a83c60262734e53de42cef
Ludwig, Lars <lars.ludwig@tu-dresden.de>
2019-04-08 17:29:39 +0200

    clear comment L2Projection

--- SOFE/core/pde/basic/L2Projection.m
+++ SOFE/core/pde/basic/L2Projection.m
@@ -4,7 +4,6 @@ classdef L2Projection < PDE
       if ~isempty(varargin), fesTest = varargin{:}; else, fesTest = fesTrial; end
       try coef = data.c; catch, coef = 1.0; end
       opList = {OpIdId(coef, 0, fesTrial, varargin{:}), FcId(data.f, fesTest, 0)};
-%       opList = {OpMatIdId(coef, 0, fesTrial, varargin{:}), FcId(data.f, fesTest, 0)};
       lhs.sys = {{1}}; rhs.sys = {{2}};
       obj = obj@PDE(opList, lhs, rhs);
     end
