9e9fa11f2483a42e4a9e37e210adc88b8c038605
Ludwig, Lars <lars.ludwig@tu-dresden.de>
2019-06-17 16:54:12 +0200

    optimization compile flags

--- SOFE/core/helpers/stenglib/compile.m
+++ SOFE/core/helpers/stenglib/compile.m
@@ -2,8 +2,8 @@ tmp = pwd;
 cd([SOFE.getCorePath() '/helpers/stenglib/']);
 %
 if ~exist('OCTAVE_VERSION', 'builtin') % Matlab
-  mex tprod.c -lmwblas
-  mex fsparse.c -lmwblas
+  mex CFLAGS='$CFLAGS -std=c99 -o2 -march=native' tprod.c -lmwblas
+  mex CFLAGS='$CFLAGS -std=c99 -o2 -march=native' fsparse.c -lmwblas
 else % Octave
   mex -std=c99 tprod.c
   mex -std=c99 fsparse.c
